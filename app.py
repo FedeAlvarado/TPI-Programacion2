9248f56edb244c72750308013ecbc3a794c11177
agustincoronel1 <agustincoronelb@gmail.com>
2023-10-20 18:17:58 -0300

    avance 5

--- app.py
+++ app.py
@@ -60,108 +60,104 @@ while True:
                 menu_alumno()
                 opt_alumno = input("Ingrese una opción de estudiante: ")
 
-                cursos_ordenados = sorted(cursos, key=lambda x: x.nombre)
-
                 if opt_alumno == "1":
                     print("Cursos disponibles:")
-                    for i, curso in enumerate(cursos_ordenados, start=1):
+                    for i, curso in enumerate(cursos, start=1):
                         print(f"{i}. {curso.nombre}")
                     curso_seleccionado = None
                     while curso_seleccionado is None:
                         try:
                             opcion = int(input("Seleccione el número del curso: "))
-                            if 1 <= opcion <= len(cursos_ordenados):
-                                curso_seleccionado = cursos_ordenados[opcion - 1]
+                            if 1 <= opcion <= len(cursos):
+                                curso_seleccionado = cursos[opcion - 1]
 
-                                if curso_seleccionado not in alumno_encontrado.matricular_curso:  # Verificar si ya está matriculado
-                                    password_matriculacion = input(f"Ingrese la contraseña de matriculación para '{curso_seleccionado.nombre}': ")
-                                    if password_matriculacion == curso_seleccionado.password:
-                                        alumno_encontrado.matricular_curso.append(curso_seleccionado)  # Agregar el curso a mi_cursos
-                                        print(f"Te has matriculado en el curso {curso_seleccionado.nombre}.")
-                                    else:
-                                        print("Contraseña de matriculación incorrecta. Intente de nuevo.")
+                                if curso_seleccionado not in alumno_encontrado.mis_cursos:
+                                    alumno_encontrado.mis_cursos.append(curso_seleccionado)
+                                    print(f"Te has matriculado en el curso {curso_seleccionado.nombre}.")
                                 else:
                                     print("Ya estás matriculado en este curso.")
+
                             else:
                                 print("Opción no válida. Intente de nuevo.")
                         except ValueError:
                             print("Ingrese un número válido.")
 
                 elif opt_alumno == "2":
-                    cursos_inscriptos = alumno_encontrado.matricular_curso
+                    cursos_inscriptos = alumno_encontrado.mis_cursos
                     for curso in cursos_inscriptos:
-                        print(f"Usted esta dictando los siguientes cursos: {curso}")
+                        print(f"Usted está inscrito en el curso: {curso.nombre}")
+
   
 
                 elif opt_alumno == "3":
                      break  # volver al menú principal
 
-        else:
-            print("Error: Correo electrónico o contraseña incorrectos.")
+            else:
+                print("Error: Correo electrónico o contraseña incorrectos.")
 
-    elif opt == "2":
-        os.system('cls')
+        elif opt == "2":
+            os.system('cls')
 
-        email = input("Ingrese su correo electrónico: ")
-        password = input("Ingrese su contraseña: ")
-        profe_encontrado = None
-        for profesor in profesores:
-            if isinstance(profesor, Profesor )  and profesor.validar_credenciales(email, password):
-                profesor_encontrado = profesor
-                break
-        if profesor_encontrado:
-            print(f"Bienvenido, {profesor_encontrado.name} {profesor_encontrado.surname} (profesor).")
+            email = input("Ingrese su correo electrónico: ")
+            password = input("Ingrese su contraseña: ")
+            profe_encontrado = None
+            for profesor in profesores:
+                if isinstance(profesor, Profesor )  and profesor.validar_credenciales(email, password):
+                    profesor_encontrado = profesor
+                    break
+            if profesor_encontrado:
+                print(f"Bienvenido, {profesor_encontrado.name} {profesor_encontrado.surname} (profesor).")
 
-            while True:
-                menu_profesor()
-                opt_profesor = input("Ingrese una opción de profesor: ")
-                cursos_ordenados = sorted(cursos, key=lambda x: x.nombre)
-                if opt_profesor == "1":
-                    print("Cursos disponibles:")
-                    for i, curso in enumerate(cursos_ordenados, start=1):
-                        print(f"{i}. {curso.nombre}")
-                    curso_seleccionado = None
-                    while curso_seleccionado is None:
-                        try:
-                            opcion = int(input("Seleccione el número del curso que desea dictar: "))
-                            if 1 <= opcion <= len(cursos_ordenados):
-                                curso_seleccionado = cursos_ordenados[opcion - 1]
+                while True:
+                    menu_profesor()
+                    opt_profesor = input("Ingrese una opción de profesor: ")
+                    cursos_ordenados = sorted(cursos, key=lambda x: x.nombre)
+                    if opt_profesor == "1":
+                        print("Cursos disponibles:")
+                        for i, curso in enumerate(cursos_ordenados, start=1):
+                            print(f"{i}. {curso.nombre}")
+                        curso_seleccionado = None
+                        while curso_seleccionado is None:
+                            try:
+                                opcion = int(input("Seleccione el número del curso que desea dictar: "))
+                                if 1 <= opcion <= len(cursos_ordenados):
+                                    curso_seleccionado = cursos_ordenados[opcion - 1]
 
-                                if curso_seleccionado not in profesor_encontrado.dicto_curso:  # Verificar si ya está matriculado
-                                    password_matriculacion = input(f"Ingrese la contraseña de matriculación para '{curso_seleccionado.nombre}': ")
-                                    if password_matriculacion == curso_seleccionado.password:
-                                        profesor_encontrado.dicto_curso.append(curso_seleccionado)  # Agregar el curso a mi_cursos
-                                        print(f"Te has subscripto para dictar el curso {curso_seleccionado.nombre}.")
+                                    if curso_seleccionado not in profesor_encontrado.dicto_curso:  # Verificar si ya está matriculado
+                                        password_matriculacion = input(f"Ingrese la contraseña de matriculación para '{curso_seleccionado.nombre}': ")
+                                        if password_matriculacion == curso_seleccionado.password:
+                                            profesor_encontrado.dicto_curso.append(curso_seleccionado)  # Agregar el curso a mi_cursos
+                                            print(f"Te has subscripto para dictar el curso {curso_seleccionado.nombre}.")
+                                        else:
+                                            print("Contraseña de matriculación incorrecta. Intente de nuevo.")
                                     else:
-                                        print("Contraseña de matriculación incorrecta. Intente de nuevo.")
+                                        print("Ya estás dictando este curso.")
                                 else:
-                                    print("Ya estás dictando este curso.")
-                            else:
-                                print("Opción no válida. Intente de nuevo.")
-                        except ValueError:
-                            print("Ingrese un número válido.")
-                    pass
+                                    print("Opción no válida. Intente de nuevo.")
+                            except ValueError:
+                                print("Ingrese un número válido.")
+                        pass
 
 
-                elif opt_profesor == "2":
-                    cursos_dictados = profesor_encontrado.dicto_curso
-                    for curso in cursos_dictados:
-                        print(f"Usted esta dictando los siguientes cursos: {curso}")
-                                        
-                elif opt_profesor == "3":
-                    break  # volver al menú principal
+                    elif opt_profesor == "2":
+                        cursos_dictados = profesor_encontrado.dicto_curso
+                        for curso in cursos_dictados:
+                            print(f"Usted esta dictando los siguientes cursos: {curso}")
+                                            
+                    elif opt_profesor == "3":
+                        break  # volver al menú principal
 
-        else:
-            print("Error: Correo electrónico o contraseña incorrectos.")
-    elif opt == "3":
-        os.system('cls')
+            else:
+                print("Error: Correo electrónico o contraseña incorrectos.")
+        elif opt == "3":
+            os.system('cls')
 
-        cursos_ordenados = sorted(cursos, key=lambda x: x.nombre)
-        cursos_ordenados = obtener_cursos_ordenados()
-        for curso in cursos_ordenados:
-            print(curso)
-            
-    elif opt == "4":
-        print("Saludos!.")
-        break
+            cursos_ordenados = sorted(cursos, key=lambda x: x.nombre)
+            cursos_ordenados = obtener_cursos_ordenados()
+            for curso in cursos_ordenados:
+                print(curso)
+                
+        elif opt == "4":
+            print("Saludos!.")
+            break
 
--- estudiante.py
+++ estudiante.py
@@ -1,26 +1,24 @@
-from usuario import *
-from curso import *
-
+from usuario import Usuario
+from curso import Curso
 
 class Estudiante(Usuario):
     def __init__(self, nombre, apellido, email, password, legajo, date_ins):
         super().__init__(nombre, apellido, email, password)
         self.__legajo = legajo
         self.__date_ins = date_ins
-        self.__mis_cursos = []  
+        self.__mis_cursos = []
 
     @property
     def legajo(self) -> str:
         return self.__legajo
+
     @property
     def date_ins(self) -> int:
         return self.__date_ins
+
     @property
-    def matricular_curso(self):
+    def mis_cursos(self):
         return self.__mis_cursos
-    @matricular_curso.setter
-    def matricular_curso(self, curso: object):
-        self.__mis_cursos = curso
 
     @legajo.setter
     def legajo(self, legajo: int):
@@ -29,10 +27,20 @@ class Estudiante(Usuario):
     @date_ins.setter
     def date_ins(self, date) -> int:
         self.__date_ins = date
-    
+
     def __str__(self) -> str:
         return f'Legajo: {self.legajo} Fecha de Inscripción: {self.date_ins}'
 
-    def matriculacion(self, curso:Curso):
-        self.matricular_curso.append(curso)
-    
+    def matricular_curso(self, curso: Curso):
+        self.__mis_cursos.append(curso)
+
+    @legajo.setter
+    def legajo(self, legajo: int):
+        self.__legajo = legajo
+
+    @date_ins.setter
+    def date_ins(self, date) -> int:
+        self.__date_ins = date
+
+    def __str__(self) -> str:
+        return f'Legajo: {self.legajo} Fecha de Inscripción: {self.date_ins}'
